871cd7c77019c9dba85ed2647c2fab6e132578e7
SIRO <109188471+sirocat@users.noreply.github.com>
2024-04-04 16:07:08 +0900

    numbercount

--- MainActivity.java
+++ MainActivity.java
@@ -1,59 +1,41 @@
-package com.example.bmi_calculator;
+package com.example.numbercounter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    EditText heightEditText,weightEditText;
-    Button resultButton;
-    TextView resultText;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    TextView numberTextView;
+    Button resetButton,plusButton;
+    int number=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        heightEditText = findViewById(R.id.heightEditText);
-        weightEditText = findViewById(R.id.weightEditText);
-        resultButton = findViewById(R.id.resultButton);
-        resultText = findViewById(R.id.resultText);
+        numberTextView = findViewById(R.id.numberTextView);
+        resetButton = findViewById(R.id.resetButton);
+        plusButton = findViewById(R.id.plusButton);
 
-        resultButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String str_height = heightEditText.getText().toString();
-                String str_weight = weightEditText.getText().toString();
+        resetButton.setOnClickListener(this);
+        plusButton.setOnClickListener(this);
+    }
 
-                if(str_height.isEmpty() || str_weight.isEmpty()){
-                    Toast.makeText(MainActivity.this, "빈값이 있습니다", Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                int height = Integer.parseInt(str_height);
-                int weight = Integer.parseInt(str_weight);
-                double bmi = weight/Math.pow(height/100.0,2.0);
-
-                resultText.setVisibility(view.VISIBLE);
-
-                if(bmi > 35)
-                    resultText.setText("고도비만");
-                else if(bmi >= 30.0)
-                    resultText.setText("중정도비만");
-                else if(bmi >= 25.0)
-                    resultText.setText("경도비만");
-                else if(bmi >= 23.0)
-                    resultText.setText("과체중");
-                else if(bmi >= 18.5)
-                    resultText.setText("정상체중");
-                else
-                    resultText.setText("저체중");
-            }
-        });
+    @Override
+    public void onClick(View view) { //이벤트 처리
+        switch (view.getId()){
+            case R.id.resetButton:
+                number = 0;
+                numberTextView.setText(Integer.toString(number));
+                break;
+            case R.id.plusButton:
+                number += 1;
+                numberTextView.setText(Integer.toString(number));
+                break;
+        }
     }
 }
